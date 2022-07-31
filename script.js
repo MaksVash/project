11e764c2a87ff3fd766f67f1c6557547d4667d5f
MacsVashuk <maximka.vashuk@gmail.com>
2022-07-31 14:11:13 +0300

    movies

--- script.js
+++ script.js
@@ -7,4 +7,9 @@ let personalMuvieDB={
     actors:{},
     ganres:[],
     private:false,
-}
+};
+
+let lastFilm=prompt('what last film did you see?','');
+let reiting=prompt('how match did you like it','');
+
+let movies ={lastFilm:reiting};
