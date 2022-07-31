498aab7f7bdfce71ae33f0931f3d60a4a33940e5
MacsVashuk <maximka.vashuk@gmail.com>
2022-07-31 13:18:14 +0300

    personalMuvieDB

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-let numberOfFilm=prompt('How match films did you see?','')
-console.log(numberOfFilm)
+let numberOfFilm=prompt('How match films did you see?','');
+console.log(numberOfFilm);
+
+let personalMuvieDB={
+    count:numberOfFilm,
+    movies:{},
+    actors:{},
+    ganres:[],
+    private:false,
+}
