e55759f763beb12f8da8414bed11638fbbd9eaab
MacsVashuk <maximka.vashuk@gmail.com>
2022-08-02 22:08:43 +0300

    cikle

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let numberOfFilm=+prompt('How match films did you see?','');
-;
+
+if(numberOfFilm<=10 && numberOfFilm>0) {
+    alert('Not match');
+}else if(numberOfFilm>10 && numberOfFilm<=30){
+alert('Normal')
+}else if(numberOfFilm>30){
+    alert('You are cinoman');
+}else{
+    alert('ERROR')
+}
 
 let personalMuvieDB={
     count:numberOfFilm,
@@ -12,14 +21,26 @@ let personalMuvieDB={
 
 
 
-let a=prompt('what last film did you see?','Film'),
-b=+prompt('how match did you like it','1-10'),
-c=prompt('what last film did you see?','Film'),
-d=+prompt('how match did you like it','1-10');
 
-personalMuvieDB.movies[a]=b;
-personalMuvieDB.movies[c]= d;
+for (let i=0;i<2;i++){
+    let a=prompt('what last film did you see?','Film'),
+    b=+prompt('how match did you like it','1-10');
+    if(a !=null && b !=null && a !='' &&  b !='' && a.length<50  ){
+    personalMuvieDB.movies[a]=b;
+console.log('done');
+}else{
+    console.log('error');
+    i--;
+}
+}
+
+
+{}
 
 
 
-console.log(personalMuvieDB);
+
+console.log(personalMuvieDB); 
+
+
+ 
